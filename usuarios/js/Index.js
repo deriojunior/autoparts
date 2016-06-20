0b215adf7ed89675d02d05dc179f4f1d109cba6b
mayconferreira <maycon.univas@gmail.com>
2016-06-20 06:53:29 -0300

    usuarios-html-bootstrap-javascript-localstorage

---
+++ usuarios/js/Index.js
@@ -0,0 +1,89 @@
+
+var Index = {
+
+	init: function() {
+		Index.setForm();
+		Index.listUsuarios();
+	},
+
+	setForm: function() {
+		var form = document.getElementById('form');
+		if(form) {
+			form.onsubmit = function() {
+				Index.saveUsuario(form);
+				return false;
+			};
+		}
+	},
+
+	saveUsuario: function(form) {
+		var usuario = {};
+		usuario.nome  = form.nome.value;
+		usuario.telefone = form.telefone.value;
+		usuario.email = form.email.value;
+		usuario.cidade = form.cidade.value;
+		usuario.estado = form.estado.value;
+		
+		if(UsuarioDAO.save(usuario) == UsuarioDAO.NEW) {
+			TableController.addItem(usuario, Index.edit, Index.delete);
+		}
+		else {
+			TableController.clearList();
+			Index.listUsuarios();
+		}
+
+		form.nome.value = form.telefone.value = form.email.value = form.cidade.value = form.estado.value = "";
+	},
+
+	setTable: function() {
+		var table = document.getElementById('tabela-usuarios');
+		TableController.setTable(table);
+	},
+
+	listUsuarios: function() {
+		Index.setTable();
+		var usuarioList = UsuarioDAO.retrieve();
+		if (usuarioList && usuarioList.length) {
+			TableController.addList(usuarioList, Index.edit, Index.delete);
+		}
+	},
+
+	mascara: function (t, mask){
+		var i = t.value.length;
+		var saida = mask.substring(1,0);
+		var texto = mask.substring(i)
+		if (texto.substring(0,1) != saida){
+			t.value += texto.substring(0,1);
+		}
+	}, 
+
+	edit: function(nome) {
+		if(confirm("Você deseja editar o usuário " + nome + " ?")) {
+			var usuario = UsuarioDAO.get(nome);
+			if (comprador) {
+				var form = document.getElementById('form');
+				form.nome.value  = usuario.nome;
+				form.telefone.value = usuario.telefone;
+				form.email.value = usuario.email;
+				form.cidade.value = usuario.cidade;
+				form.estado.value = usuario.estado;
+			}
+		}
+	},
+
+	delete: function(nome, element) {
+		if(confirm("Você deseja deletar o usuário " + nome + " ?")) {
+			var usuario = UsuarioDAO.get(nome);
+			if (usuario) {
+				if(UsuarioDAO.delete(nome)) {
+					var row = element.parentNode.parentNode;
+					row.parentNode.removeChild(row);
+				}
+			}	
+		}
+	}
+};
+
+//initialization
+UsuarioDAO.unserializeAndParse();
+Index.init();
---
+++ usuarios/js/UsuarioDAO.js
@@ -0,0 +1,96 @@
+
+var UsuarioDAO = {
+
+	DB_KEY: "usuarios",
+	NEW: 1,
+	UPDATE: 2,
+
+	list: [], 
+
+	save: function(usuario, tableController) {
+		var list  = UsuarioDAO.list,
+		    index = UsuarioDAO.getIndex(usuario);
+		
+		if(index > -1) {
+			list[index] = usuario;
+			UsuarioDAO.serializeAndSave();
+			return UsuarioDAO.UPDATE;
+		}
+		else {
+			list.push(usuario);
+			if(tableController) {
+				tableController.addItem(usuario);
+			}
+		}
+		
+		UsuarioDAO.serializeAndSave();
+
+		return UsuarioDAO.NEW;
+	},
+
+	retrieve: function() {
+		var list = UsuarioDAO.list;
+		if(list && list.length > 0) {
+			return list;
+		}
+		return null;
+	},
+
+	get: function(nome) {
+		var list  = UsuarioDAO.list,
+		    index = UsuarioDAO.getIndex({'nome': nome});
+
+		if (index > -1) {
+			var usuario = list[index];
+			return usuario;
+		}
+
+		return null;
+	},
+
+	getIndex: function(usuario) {
+		var list = UsuarioDAO.list,
+		    item = {};
+
+		for (var i = 0; i < list.length; i++) {
+			item = list[i];
+			if(item.nome == usuario.nome) {
+				return i;
+			}
+		}
+
+		return -1;
+	},
+
+	delete: function(nome) {
+		var list  = UsuarioDAO.list,
+		    index = UsuarioDAO.getIndex({'nome': nome});
+
+		if (index > -1) {
+			list.splice(index, 1);
+			UsuarioDAO.serializeAndSave();
+			return true;
+		}
+
+		return false;
+	},
+
+	serializeAndSave: function() {
+		var list = UsuarioDAO.list;
+		if(list && list.length > 0) {
+			var json = JSON.stringify(UsuarioDAO.list);
+			window.localStorage.setItem(UsuarioDAO.DB_KEY, json);
+		}
+	},
+
+	unserializeAndParse: function() {
+		var json = window.localStorage.getItem(UsuarioDAO.DB_KEY);
+		if(json) {
+			UsuarioDAO.list = JSON.parse(json);
+		}
+		else {
+			UsuarioDAO.list = [];
+		}
+	}
+
+};
